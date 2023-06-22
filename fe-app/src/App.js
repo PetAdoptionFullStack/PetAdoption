123d2f9869f6a64c24e5a20e618e8e62458f2a0d
crisncris0000 <christopherrivera384@gmail.com>
2023-06-22 08:43:33 -0400

    Added Navbar component

--- fe-app/src/App.js
+++ fe-app/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+
 
 function App(){
-
-  function getAPI() {
-    fetch('http://localhost:9000/testAPI')
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      console.log(data);
-    })
-  }
-  getAPI();
-
   return (
     <div className="App">
-      <h1>Hello</h1>
+      
     </div>
   );
 }
