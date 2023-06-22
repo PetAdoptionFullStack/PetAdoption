b46544a0ba176b5083b89a727c7033426850c351
crisncris0000 <christopherrivera384@gmail.com>
2023-06-21 21:48:26 -0400

    Added a testAPI to test connection between the frontend and backend

--- fe-app/src/App.js
+++ fe-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+
+  function getAPI() {
+    fetch('http://localhost:9000/testAPI')
+    .then(response => {
+      return response.text();
+    })
+    .then(data => {
+      console.log(data);
+    })
+  }
+  getAPI();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
