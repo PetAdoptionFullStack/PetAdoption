7b3b24165a70ad4e7e1dff44582c02cd4363790c
crisncris0000 <christopherrivera384@gmail.com>
2023-06-30 15:27:32 -0400

    Commented out the html code

--- fe-app/src/components/logs.js
+++ fe-app/src/components/logs.js
@@ -1,21 +1,21 @@
-const loginButton = document.getElementById("loginButton");
-const signupButton = document.getElementById("signupButton");
-const loginPopup = document.getElementById("loginPopup");
-const signupPopup = document.getElementById("signupPopup");
+// const loginButton = document.getElementById("loginButton");
+// const signupButton = document.getElementById("signupButton");
+// const loginPopup = document.getElementById("loginPopup");
+// const signupPopup = document.getElementById("signupPopup");
 
-//show/hide fn
+// //show/hide fn
 
-function togglePopup(popup) {
-    popup.classList.toggle("show");
-  }
+// function togglePopup(popup) {
+//     popup.classList.toggle("show");
+//   }
 
-  //event
-  loginButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    togglePopup(loginPopup);
-  });
+//   //event
+//   loginButton.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     togglePopup(loginPopup);
+//   });
   
-  signupButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    togglePopup(signupPopup);
-  });
+//   signupButton.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     togglePopup(signupPopup);
+//   });
